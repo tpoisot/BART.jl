ed2a93c1ef312a1ea22e626ead565c515a53d7f0
Timothée Poisot <timothee.poisot@umontreal.ca>
2024-04-26 10:54:04 -0400

    quick experiment on prediction

--- _test.jl
+++ _test.jl
@@ -3,15 +3,29 @@ using BART
 
 y, X = BART.bigfoot()
 
+# This is an example
+x = X[3,:]
+
 # Do the initial tree
 tree = Tree(y, X)
 
-# We split a node with
-while depth(tree) < 6
-    split!(rand(leaves(tree)), tree)
+# Let's make some bad splits
+while depth(tree) < 4
+    spl = rand(leaves(tree))
+    while length(spl.pool) < 5
+        spl = rand(leaves(tree))
+    end
+    split!(spl, tree)
 end
 
-# Probability that a node is non terminal
-α = 0.5
-d = depth.(leaves(tree))
-α.^d
+function g(x::Vector, node::DecisionNode)
+    if BART.isterminal(node)
+        return node.decision
+    end
+    return x[node.feature] .<= node.value ? g(x, node.left) : g(x, node.right)
+end
+g(x::Vector, tree::Tree) = g(x, tree.root)
+g(X::Matrix, node::DecisionNode) = mapslices(x -> g(x, node), X, dims=2)
+g(X::Matrix, tree::Tree) = g(X, tree.root)
+
+g(X, tree)
